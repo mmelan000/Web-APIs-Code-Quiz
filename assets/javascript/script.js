45bc9a47986afc5f97f5c1a315d3bb0ed90b2a3f
Michael Melanson <m.melanson000@gmail.com>
2022-06-20 16:10:05 -0400

    testing functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+var questionBox = document.querySelector("#id")
+console.log(questionBox)
+
+setCont
+
+function quizzer(question, right, wrong1, wrong2, wrong3) {
+    questionBox.appendchild[0].setContent(question)
+questionBox
+    // display question
+// array 1-4 rand
+// assign correct answer, remove number from index, assign wrong asnswer remove from index, repeat
+
+}
+
+quizzer (test, 1, 2, 3, 4);
